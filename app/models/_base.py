7f1a3889e4f4d5450a646a7af39f2aa21e33cf63
ck <chenke91@qq.com>
2014-11-18 10:03:14 +0800

    run

---
+++ app/models/_base.py
@@ -0,0 +1,22 @@
+
+import datetime
+
+class SessionMixin(object):
+    def to_dict(self, *columns):
+        dct = {}
+        for col in columns:
+            value = getattr(self, col)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%Y-%m-%d %H:%M:%S')
+            dct[col] = value
+        return dct
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+        return self
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+        return self
